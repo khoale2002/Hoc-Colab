fe8b11eb872120b67ae343a0163d050df8608852
khoa2210 <73996278+khoa2210@users.noreply.github.com>
2020-11-22 19:50:48 +0700

    Add files via upload

---
+++ bta.py
@@ -0,0 +1,9 @@
+print("Chuong trinh giai pt bac 1")
+n = int(input("so lan ban muon lap ="))
+for i in range(int(n)):
+    a = int(input('a = '))
+    b = int(input('b = '))
+    if a == 0 :
+        print("vo nghiem")
+    else :
+        print(-b/a)
