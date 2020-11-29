7119c42c952c78676a139a0658631b82f6f7c4cf
khoa2210 <73996278+khoa2210@users.noreply.github.com>
2020-11-29 16:24:25 +0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,9 @@
+import string,random
+list = []
+letters = string.ascii_letters
+for i in range((random.randint(50,100))) :
+ dict = {}
+ dict['name'] =''.join(random.choice(letters) for i in range(random.randint(1, 10)))
+ dict["age"] = random.randint(0,100)
+ list.insert(0, dict)
+print(list)
