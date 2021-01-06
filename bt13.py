ba246360cd4d3efbe17f7b4aacb750cd558acb24
khoa2210 <73996278+khoa2210@users.noreply.github.com>
2021-01-06 14:49:31 +0700

    Update bt13.py

--- bt13.py
+++ bt13.py
@@ -1,14 +1,15 @@
 import os
-list1=list()
-list2=list()
-for (root,dirs,files) in os.walk('C:'):
-    print("thu muc o dia C:",root)
-    print("thu muc con",dirs)
-    print("tap tin ",files)
-for (root,dirs,files) in os.walk('C:'):
-    for i in files:
-        list1.append(i)
-    for x in dirs:
-        list2.append(x)
-print("list tap tin ",list1)
-print("list thu muc con ",list2)
+
+print("tất cả các tệp và thư mục trong ổ C:")
+path = 'C:\\Users\\MyPC\\Documents'
+print(os.listdir(path))
+print("")
+
+print("các thư mục:")
+list1 = next(os.walk(path))[1]
+print(list1)
+print("")
+
+print("các tệp:")
+list2 = next(os.walk(path))[2]
+print(list2)
